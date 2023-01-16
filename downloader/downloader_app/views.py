cbf636c2d2117d0c96cc2fed799ecbe4087743f3
deansainer <determind2k@gmail.com>
2023-01-17 00:29:10 +0100

    commit

--- downloader/downloader_app/views.py
+++ downloader/downloader_app/views.py
@@ -6,7 +6,9 @@ def index(request):
     if request.method == 'POST':
         link = request.POST['link']
         video = YouTube(link)
-        stream = video.streams.get_lowest_resolution()
+        if 'download_low' in request.POST:
+            stream = video.streams.get_lowest_resolution()
+        elif 'download_high' in request.POST:
+            stream = video.streams.get_highest_resolution()
         stream.download()
-
     return render(request, 'downloader_app/index.html')
